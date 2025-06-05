5a14a5b8a2fc9cadca30417c18772a5e43446035
LokyV1 <luigilok24@gmail.com>
2025-06-05 08:08:09 +0200

    correzioni

--- src/main/java/it/apulia/ecommerce/cornershop/service/RuoloService.java
+++ src/main/java/it/apulia/ecommerce/cornershop/service/RuoloService.java
@@ -1,30 +1,31 @@
 package it.apulia.ecommerce.cornershop.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
 import it.apulia.ecommerce.cornershop.domain.Ruolo;
 import it.apulia.ecommerce.cornershop.repository.RuoloRepository;
 import it.apulia.ecommerce.cornershop.rest.dto.RuoloDTO;
-import lombok.Getter;
-import lombok.Setter;
-
-
 
 @Service
 public class RuoloService extends AService<String, Ruolo, RuoloDTO>{
 
-    public RuoloService(RuoloRepository repo){
+    public RuoloService(RuoloRepository repo) {
         super(repo);
     }
-
+ 
     @Override
-    protected Ruolo map(RuoloDTO dto){
-        throw new UnsupportedOperationException("unimplemented method 'map'");
+    protected Ruolo map(RuoloDTO dto) {
+        Ruolo ruolo = new Ruolo();
+        ruolo.setNome(dto.getNome());
+        ruolo.setCodice(dto.getCodice());
+        return ruolo;
     }
 
     @Override
-    protected Ruolo map(Ruolo entity, RuoloDTO dto){
-        throw new UnsupportedOperationException("unimplemented method 'map'");
+    protected Ruolo map(Ruolo entity, RuoloDTO dto) {
+        entity.setNome(dto.getNome());
+        entity.setCodice(dto.getCodice());
+        return entity;
     }
     
 }
