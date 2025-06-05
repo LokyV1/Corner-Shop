4590c08d79de828f3f6788803098fa2961cc895c
LokyV1 <luigilok24@gmail.com>
2025-06-05 10:49:20 +0200

    Controller aggiornati

--- src/main/java/it/apulia/ecommerce/cornershop/rest/AcquistoController.java
+++ src/main/java/it/apulia/ecommerce/cornershop/rest/AcquistoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import it.apulia.ecommerce.cornershop.domain.Acquisto;
 import it.apulia.ecommerce.cornershop.rest.dto.AcquistoDTO;
 import it.apulia.ecommerce.cornershop.rest.dto.AcquistoResponseDTO;
+import it.apulia.ecommerce.cornershop.rest.dto.RuoloDTO;
+import it.apulia.ecommerce.cornershop.rest.dto.RuoloResponseDTO;
 import it.apulia.ecommerce.cornershop.service.AcquistoService;
 import lombok.RequiredArgsConstructor;
 
@@ -59,5 +62,6 @@ public class AcquistoController {
     public void deleteById(@PathVariable String id) throws Exception {
         acquistoService.deleteById(id);
     }
+    
 
 }
--- src/main/java/it/apulia/ecommerce/cornershop/rest/RuoloController.java
+++ src/main/java/it/apulia/ecommerce/cornershop/rest/RuoloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -60,7 +61,6 @@ public class RuoloController {
     public void deleteById(@PathVariable String id) throws Exception {
         ruoloService.deleteById(id);
     }
-
 
 
 }
