d6451eb4488bd7333e942050679ea05fc24ec9ef
LokyV1 <luigilok24@gmail.com>
2025-06-05 09:18:35 +0200

    correzione di DipendenteService

--- src/main/java/it/apulia/ecommerce/cornershop/service/DipendenteService.java
+++ src/main/java/it/apulia/ecommerce/cornershop/service/DipendenteService.java
@@ -1,11 +1,13 @@
 package it.apulia.ecommerce.cornershop.service;
 
 
+import org.springframework.stereotype.Service;
+
 import it.apulia.ecommerce.cornershop.domain.Dipendente;
 import it.apulia.ecommerce.cornershop.repository.DipendenteRepository;
 import it.apulia.ecommerce.cornershop.rest.dto.DipendenteDTO;
 
-
+@Service
 public class DipendenteService extends AService<String, Dipendente, DipendenteDTO>{
     
 
